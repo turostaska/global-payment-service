1c0aac12148096ab9cf4bde6264ac064722d7e7a
Albert Radó <rado.albert@gmail.com>
2026-02-20 15:59:15 +0100

    initial commit

---
+++ src/main/java/com/example/global_payment_service/GlobalPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.global_payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalPaymentServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GlobalPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/global_payment_service/GlobalPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.global_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
